2674e93f61da4373e8492744da6843f4e6c6b33a
Đình Phúc <38375962+dinhphuc1309@users.noreply.github.com>
2022-11-17 02:11:17 +0700

    tự động đăng nhập

--- lib/controllers/splash_controller.dart
+++ lib/controllers/splash_controller.dart
@@ -19,9 +19,18 @@ class SplashController extends GetxController {
       await saveKeyGoogleMap(googleMapApi);
       google_map_api_key =
           await BaseSharedPreferences.getString('key_google_map');
-      Get.offNamed(RoutesClass.login);
+      checkLogin();
     } else {
       print("Dữ liệu tài khoản chưa được đổ vào!");
+    }
+  }
+
+  checkLogin() async {
+    String idKhachHang = await BaseSharedPreferences.getString('id_khachhang');
+    if (idKhachHang != '') {
+      Get.offNamed(RoutesClass.home);
+    } else {
+      Get.offNamed(RoutesClass.login);
     }
   }
 }
